ccf691cad7c66e33ec6a642491e931e62bc19669
LimiO <52657506+LimiO@users.noreply.github.com>
2020-06-19 20:36:35 +0300

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,11 +1,6 @@
 #!/usr/local/bin/python3.8
-import os
-import sys
 import db
 
-print('something')
 users = db.get_users()
-print(users)
 for user in users:
-    print(user.id)
     user.reset_limits()
